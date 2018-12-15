b414fc1cb162400690e1a7238fe3f6012cab2338
Eamon Ramstein <lordeamon@gmail.com>
2018-12-15 03:58:20 +0100

    VoteButton clickAble now check if vote have been cast. if so button isnt clickable .

--- app/src/main/java/dk/dampbiksen/community/util/PollCardRVAdapter.java
+++ app/src/main/java/dk/dampbiksen/community/util/PollCardRVAdapter.java
@@ -1,5 +1,6 @@
 package dk.dampbiksen.community.util;
 
+import android.graphics.PorterDuff;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
